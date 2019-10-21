fa25577052bcb0876639f77246ca3d0824c2b154
dev <dev@ravimoshark.com>
2019-10-21 11:02:07 +0200

    Scaping characters.

--- src/java_update_version.sh
+++ src/java_update_version.sh
@@ -26,7 +26,7 @@ if ! type "mvn" &> /dev/null; then
 fi
 
 # shellcheck disable=SC2154 disable=SC2016
-MVN_VERSION="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)"
+MVN_VERSION="$(mvn -q -Dexec.executable=echo -Dexec.args='\${project.version}' --non-recursive exec:exec)"
 
 MVN_DATE="$(date -u '+%y%m%d%H%M%S')"
 
